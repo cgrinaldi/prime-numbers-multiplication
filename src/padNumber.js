41f4f576d3869da6ebcfd9355cb6017184008acf
Chris Rinaldi <cgrinaldi@gmail.com>
2016-01-27 19:54:14 -0800

    Implement padNumber()

--- src/padNumber.js
+++ src/padNumber.js
@@ -1,5 +1,16 @@
 function padNumber(number, width) {
-  return '';
+  if (typeof number !== 'number') {
+    throw new Error('Must be provided a number');
+  }
+  if (typeof width !== 'number' || width % 1 !== 0 || width <= 0) {
+    throw new Error('Provided width must be a positive integer');
+  }
+
+  var result = String(number).split('');
+  while (result.length < width) {
+    result.unshift(' ');
+  }
+  return result.join('');
 }
 
 module.exports = padNumber;
